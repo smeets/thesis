f43ce96197ed98d200e3e14905bb2bb010c7f591
allah-1 <allah-1@allah.com>
2019-06-13 23:58:44 +0200

    cleanup

--- d3fi/data1.sh
+++ d3fi/data1.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-SL=$1
-DEVICE=$2
+DEVICE=$1
+INODE=$2
 X=0
 
-echo "time,bytes,packets,dropped,requeued,txretry,txfailed,sndbuf"
+echo "time,sent,bytes,packets,dropped,requeued,txretry,txfailed,txrate,xmit,sndbuf"
 while [ 1 == 1 ];
 do
-	DATA=$(./tcq/tcq 2>&1 | sed -e '/backlog/d' )
-	SNDBUF=$(cat /proc/net/udp | grep "${SL}:" | awk '{ q = "0x" substr($5, 1, 8); printf "%d\n", q }')
-	TXRETRY=$(ethtool -S $DEVICE | grep 'tx_retries' | awk '{ print $2 }')
-	TXFAILED=$(ethtool -S $DEVICE | grep 'tx_retry_failed' | awk '{ print $2 }')
+	DATA=$(./tcq/tcq 2>&1 | sed -e '/sent/d' )
+	SNDBUF=$(cat /proc/net/udp | grep "$INODE" | awk '{ q = "0x" substr($5, 1, 8); printf "%d\n", q }')
+	ETHSTATS=$(ethtool -S $DEVICE | awk '/tx_retries/ {RETRY=$2} /tx_retry_failed/{FAILED=$2} /txrate/{RATE=$2} /tx_packets/{SENT=$2} END{ print RETRY ", " FAILED ", " RATE ", " SENT }')
 
-	echo $X, $DATA, $TXRETRY, $TXFAILED, $SNDBUF
+	echo $X, $DATA, $ETHSTATS, $SNDBUF
 	sleep 1s
 	X=$(( X + 1 ))
 done
