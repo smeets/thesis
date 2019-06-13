a4a22f3a6b2c49ad519127421030ae1a85a533e3
allah-1 <allah-1@allah.com>
2019-06-13 15:32:24 +0200

    add netdevice to data1.sh

--- d3fi/data1.sh
+++ d3fi/data1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 SL=$1
+DEVICE=$2
 X=0
 
 echo "time,bytes,packets,dropped,requeued,txretry,txfailed,sndbuf"
@@ -7,8 +8,8 @@ while [ 1 == 1 ];
 do
 	DATA=$(./tcq/tcq 2>&1 | sed -e '/backlog/d' )
 	SNDBUF=$(cat /proc/net/udp | grep "${SL}:" | awk '{ q = "0x" substr($5, 1, 8); printf "%d\n", q }')
-	TXRETRY=$(ethtool -S wlp3s0 | grep 'tx_retries' | awk '{ print $2 }')
-	TXFAILED=$(ethtool -S wlp3s0 | grep 'tx_retry_failed' | awk '{ print $2 }')
+	TXRETRY=$(ethtool -S $DEVICE | grep 'tx_retries' | awk '{ print $2 }')
+	TXFAILED=$(ethtool -S $DEVICE | grep 'tx_retry_failed' | awk '{ print $2 }')
 
 	echo $X, $DATA, $TXRETRY, $TXFAILED, $SNDBUF
 	sleep 1s
--- d3fi/readme.md
+++ d3fi/readme.md
@@ -5,6 +5,6 @@ d3-based wifi logger
 ```bash
 $ python -m SimpleHTTPServer
 # cat /proc/net/udp --> get $SL
-$ ./data1.sh $SL > ./data1.csv
+$ ./data1.sh $SL $NETDEVICE > ./data1.csv
 # browse to localhost:8000/queueplot.html
 ```
